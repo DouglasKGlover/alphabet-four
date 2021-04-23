567383e6011bfa896e147d408ce8bce3cffde7c7
Douglas Glover <douglas.glover@outlook.com>
2021-04-22 23:30:26 -0400

    4 and y as pages, added an index

---
+++ src/pages/iv.vue
@@ -0,0 +1,249 @@
+<template>
+  <Layout>
+    <script id="vertexShader" type="x-shader/x-vertex">
+      varying vec3 vNormal;
+      void main()
+      {
+          vNormal = normalize( normalMatrix * normal );
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }
+    </script>
+
+    <script id="fragmentShader" type="x-shader/x-vertex">
+      varying vec3 vNormal;
+      void main()
+      {
+      	float intensity = pow( 0.7 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) ), 4.0 );
+          gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;
+      }
+    </script>
+
+    <canvas id="three"></canvas>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    titleTemplate: "4",
+  },
+  data() {
+    return {
+      mousePosition: {
+        x: 0,
+        y: 0,
+      },
+      canvas: {},
+      camera: {},
+      scene: {},
+      render: {},
+      composer: {},
+      loader: {},
+      models: {
+        four: {
+          rotation: {
+            x: 0,
+            y: 0,
+            z: 0,
+          },
+          position: {
+            x: 0,
+            y: 0,
+            z: 0,
+          },
+        },
+        orb: {
+          rotation: {
+            x: 0,
+            y: 0,
+            z: 0,
+          },
+          position: {
+            x: 0,
+            y: 0,
+            z: 0,
+          },
+        },
+        sun: {
+          rotation: {
+            x: 0,
+            y: 0,
+            z: 0,
+          },
+          position: {
+            x: 0,
+            y: 0,
+            z: 0,
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    init() {
+      const _self = this;
+
+      _self.canvas = document.getElementById("three");
+
+      // Render the canvas
+      _self.renderer = new _self.$THREE.WebGLRenderer({ canvas: _self.canvas });
+      this.renderer.setSize(
+        _self.canvas.clientWidth,
+        _self.canvas.clientHeight
+      );
+
+      // Camera
+      _self.camera = new _self.$THREE.PerspectiveCamera(75, 1, 0.1, 2000);
+      _self.camera.position.z = 10;
+      _self.camera.position.y = -1;
+      _self.camera.aspect =
+        _self.canvas.clientWidth / _self.canvas.clientHeight;
+      _self.camera.updateProjectionMatrix();
+
+      // Scene
+      _self.scene = new _self.$THREE.Scene();
+
+      // 4
+      _self.loader = new _self.$GLTFLoader();
+      _self.loader.load("./4.glb", function(gltf) {
+        _self.models.four = gltf.scene;
+        _self.scene.add(_self.models.four);
+      });
+
+      // Spheres
+      var customMaterial = new _self.$THREE.ShaderMaterial({
+        uniforms: {},
+        vertexShader: document.getElementById("vertexShader").textContent,
+        fragmentShader: document.getElementById("fragmentShader").textContent,
+        side: _self.$THREE.BackSide,
+        blending: _self.$THREE.AdditiveBlending,
+        transparent: true,
+      });
+
+      const orbGeo = new _self.$THREE.SphereGeometry(2, 30, 30);
+      _self.models.orb = new _self.$THREE.Mesh(orbGeo, customMaterial);
+      _self.scene.add(_self.models.orb);
+
+      // Light
+      let directionalLight = new _self.$THREE.DirectionalLight(0xffccaa, 3);
+      directionalLight.position.set(0, 0, -1);
+      _self.scene.add(directionalLight);
+
+      // "Sun"
+      let circleGeo = new _self.$THREE.CircleGeometry(220, 50);
+      let circleMat = new _self.$THREE.MeshBasicMaterial({ color: 0xffca05 });
+      _self.models.sun = new _self.$THREE.Mesh(circleGeo, circleMat);
+      _self.models.sun.position.x = 0;
+      _self.models.sun.position.y = 100;
+      _self.models.sun.position.z = -500;
+      _self.models.sun.scale.setX(1.2);
+      _self.scene.add(_self.models.sun);
+
+      // God Rays
+      let godraysEffect = new _self.$POSTPROCESSING.GodRaysEffect(
+        _self.camera,
+        this.models.sun,
+        {
+          resolutionScale: 1,
+          density: 0.8,
+          decay: 0.95,
+          weight: 0.9,
+          samples: 100,
+        }
+      );
+
+      // Shader passes
+      let renderPass = new _self.$POSTPROCESSING.RenderPass(
+        _self.scene,
+        _self.camera
+      );
+      let effectPass = new _self.$POSTPROCESSING.EffectPass(
+        _self.camera,
+        godraysEffect
+      );
+      effectPass.renderToScreen = true;
+
+      _self.composer = new _self.$POSTPROCESSING.EffectComposer(_self.renderer);
+      _self.composer.addPass(renderPass);
+      _self.composer.addPass(effectPass);
+    },
+    getMousePosition() {
+      const _self = this;
+      const body = document.querySelector("body");
+      let rotateXIntensity = 10;
+      let rotateYIntensity = 10;
+      body.addEventListener("mousemove", (e) => {
+        _self.mousePosition.x =
+          (e.clientX / innerWidth) * rotateXIntensity - rotateXIntensity / 2;
+        _self.mousePosition.y =
+          (e.clientY / innerHeight) * rotateYIntensity - rotateYIntensity / 2;
+      });
+      body.addEventListener("touchmove", (e) => {
+        _self.mousePosition.x =
+          (e.touches[0].clientX / innerWidth) * rotateXIntensity -
+          rotateXIntensity / 2;
+        _self.mousePosition.y =
+          (e.touches[0].clientY / innerHeight) * rotateYIntensity -
+          rotateYIntensity / 2;
+      });
+    },
+    resizeRendererToDisplaySize() {
+      // const canvas = this.renderer.domElement;
+      const width = this.canvas.clientWidth;
+      const height = this.canvas.clientHeight;
+      const needResize =
+        this.canvas.width !== width || this.canvas.height !== height;
+      return needResize;
+    },
+    animate() {
+      const _self = this;
+
+      requestAnimationFrame(_self.animate);
+
+      const canvas = _self.renderer.domElement;
+      _self.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      _self.camera.updateProjectionMatrix();
+      _self.renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+
+      _self.models.four.rotation.x = (_self.mousePosition.y - 0.5) / 10;
+      _self.models.four.rotation.y = (_self.mousePosition.x - 0.5) / 10;
+
+      _self.models.orb.position.x = _self.mousePosition.y - 0.5;
+      _self.models.orb.position.y = _self.mousePosition.x - 0.5;
+
+      _self.models.sun.position.y = _self.mousePosition.y * 10 + 100;
+      _self.models.sun.position.x = _self.mousePosition.x * 10;
+
+      _self.composer.render(_self.scene, _self.camera);
+    },
+    randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+  },
+  mounted() {
+    this.getMousePosition();
+    this.init();
+    this.animate();
+  },
+};
+</script>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #111;
+}
+
+#three {
+  width: 100vw !important;
+  height: 100vh !important;
+  display: block;
+}
+</style>
---
+++ src/pages/y.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="app">
+    <div class="text-container">
+      <h1>
+        y
+        <span
+          v-for="i in 100"
+          :style="
+            'transform: translateX(' +
+              xTrans(i) +
+              'px) translateY(' +
+              yTrans(i) +
+              'px);'
+          "
+        >
+          y
+        </span>
+        <span
+          class="opposite"
+          v-for="i in 100"
+          :style="
+            'transform: translateX(' +
+              -xTrans(i) +
+              'px) translateY(' +
+              -yTrans(i) +
+              'px);'
+          "
+        >
+          y
+        </span>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Some text",
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    mouseMove() {
+      const _self = this;
+      const body = document.querySelector("body");
+      body.addEventListener("mousemove", (e) => {
+        _self.x = e.clientX;
+        _self.y = e.clientY;
+      });
+      body.addEventListener("touchmove", (e) => {
+        _self.x = e.touches[0].clientX;
+        _self.y = e.touches[0].clientY;
+      });
+    },
+    xTrans(i) {
+      return this.xCentered * ((i + 1) / 20);
+    },
+    yTrans(i) {
+      return this.yCentered * ((i + 1) / 20);
+    },
+  },
+  mounted() {
+    this.mouseMove();
+  },
+  computed: {
+    xCentered() {
+      const xCentered = (Math.round((this.x / innerWidth) * 100) - 50) * 2;
+      return xCentered;
+    },
+    yCentered() {
+      const yCentered = (Math.round((this.y / innerHeight) * 100) - 50) * 2;
+      return yCentered;
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #111;
+}
+
+.text-container {
+  position: relative;
+  pointer-events: none;
+}
+
+h1,
+span {
+  font-family: arial, helvetica, sans-serif;
+  font-size: 50vw;
+}
+
+h1 {
+  z-index: 2;
+  position: relative;
+  color: #111;
+}
+
+span {
+  z-index: -1;
+  position: absolute;
+  left: 0;
+  top: 0;
+  color: #ffca05;
+}
+span.opposite {
+  color: #fff;
+}
+</style>
